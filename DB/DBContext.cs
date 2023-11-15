62eaffaed8524c05a7a71b7464efd746f73a2a34
Ricardo-we <ricardo0406dev@gmail.com>
2023-11-14 21:24:53 -0600

    Added new models

--- DB/DBContext.cs
+++ DB/DBContext.cs
@@ -17,10 +17,13 @@ namespace Avia.DB
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
             DbPath = Path.Join(path, "aviadb.db");
-      
+
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Ciudad> Ciudades { get; set; }
+        public DbSet<Vuelo> Vuelos { get; set; }
+        public DbSet<TarjetaVuelo> TarjetasVuelo { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -33,4 +36,4 @@ namespace Avia.DB
             options.UseSqlite(@$"Data Source={DbPath}");
 
     }
-}
+}         
---
+++ DB/Vuelos.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class Ciudad
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+
+    public class Vuelo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int CiudadOrigenId { get; set; }
+        public int CiudadDestinoId { get; set; }
+        public DateTime FechaIda { get; set; }
+        public DateTime FechaVuelta { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime HoraSalida { get; set; }
+        public DateTime HoraLlegada { get; set; }
+
+        public virtual Ciudad CiudadOrigen { get; set; }
+        public virtual Ciudad CiudadDestino { get; set; }
+    }
+
+    public class TarjetaVuelo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int VueloId { get; set; }
+        public string CiudadDestinoImgUrl { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime HoraSalida { get; set; }
+        public DateTime HoraLlegada { get; set; }
+
+        public virtual Vuelo Vuelo { get; set; }
+    }
+
+}
