905fbe3dcfbadc9d32b2d5db7efb1f5e4845ba80
darwin_aceituno <dsaswi369@gmail.com>
2023-11-23 20:30:47 -0600

    VistaVuelos

---
+++ DB/MisVuelos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class MisVuelos
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public User user { get; set; }
+        public Vuelo Vuelo { get; set; }
+
+    }
+}
---
+++ WinFormsApp1/MisVuelosForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace AviaApp
+{
+    partial class MisVuelosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            pictureBox4 = new PictureBox();
+            label2 = new Label();
+            materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Navy;
+            panel2.Controls.Add(pictureBox4);
+            panel2.Controls.Add(label2);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 71);
+            panel2.TabIndex = 89;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.avia__4_;
+            pictureBox4.Location = new Point(436, 12);
+            pictureBox4.Margin = new Padding(2);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(63, 45);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 1;
+            pictureBox4.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Britannic Bold", 36F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(311, 9);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 48);
+            label2.TabIndex = 0;
+            label2.Text = "AVIA";
+            // 
+            // materialLabel1
+            // 
+            materialLabel1.AutoSize = true;
+            materialLabel1.Depth = 0;
+            materialLabel1.Font = new Font("Roboto Medium", 20F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel1.FontType = MaterialSkin.MaterialSkinManager.fontType.H6;
+            materialLabel1.Location = new Point(311, 73);
+            materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel1.Name = "materialLabel1";
+            materialLabel1.Size = new Size(188, 24);
+            materialLabel1.TabIndex = 90;
+            materialLabel1.Text = "Mi Listado de Vuelos";
+            // 
+            // MisVuelosForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(materialLabel1);
+            Controls.Add(panel2);
+            Name = "MisVuelosForm";
+            Text = "MisVuelosForm";
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private PictureBox pictureBox4;
+        private Label label2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+    }
+}
